9160e22f32ebed9e2ce8f01b607b7e1b0bee5ec4
Bo Yuan <boyuan@192-168-1-9.tpgi.com.au>
2020-01-05 14:33:37 +1100

    refactor

--- model/product/product.go
+++ model/product/product.go
@@ -2,16 +2,26 @@ package product
 
 import (
 	"dp/component"
-	"dp/entities"
+
+	"github.com/jinzhu/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	Title    string
+	Name     string
+	Price    float64
+	Quantity int
+	Status   bool
+}
+
 func init() {
-	component.DB.AutoMigrate(&entities.Product{})
+	component.DB.AutoMigrate(&Product{})
 }
 
 // FindAll find all products
-func FindAll() ([]entities.Product, error) {
-	var products []entities.Product
+func FindAll() ([]Product, error) {
+	var products []Product
 	component.DB.Find(&products)
 	return products, nil
 }
